596ba752a9c3b304f06177e094cc455200a123d1
ojiwoong <ojiwoong@gmail.com>
2022-04-08 22:02:00 +0900

    미사용 service 삭제

--- order-service/src/main/java/com/example/orderservice/service/OrderMenuService.java
+++
@@ -1,4 +0,0 @@
-package com.example.orderservice.service;
-
-public interface OrderMenuService {
-}
--- order-service/src/main/java/com/example/orderservice/service/OrderMenuServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.example.orderservice.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class OrderMenuServiceImpl implements OrderMenuService{
-}
