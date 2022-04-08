9212e26cc62c11e29bf7406677dbd1264c9206e1
ojiwoong <ojiwoong@gmail.com>
2022-04-08 17:48:22 +0900

    모든 DB 테이블 createdAt, updatedAt 자동 동기화 사용 설정
    * 각 서비스 메인 클래스에 @EnableJpaAuditing 추가

--- menu-service/src/main/java/com/example/menuservice/MenuServiceApplication.java
+++ menu-service/src/main/java/com/example/menuservice/MenuServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.menuservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MenuServiceApplication {
 
     public static void main(String[] args) {
